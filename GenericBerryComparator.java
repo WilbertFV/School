f47327302fa4fc908a499f09b507abe55267291f
WilbertFV <villalow@kean.edu>
2024-03-08 00:18:23 -0500

    Deleted Berry + Generic Berry with issues

--- GenericBerryComparator.java
+++
@@ -1,9 +0,0 @@
-import java.time.LocalDate;
-import java.util.Comparator;
-
-public class GenericBerryComparator implements Comparator<GenericBerry<Double, LocalDate>> {
-    @Override
-    public int compare(GenericBerry<Double, LocalDate> o1, GenericBerry<Double, LocalDate> o2) {
-        return o1.getPrice().compareTo(o2.getPrice());
-    }
-}
