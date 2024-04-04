dace8637b180a0b6017568b90cb222df68a57489
WilbertFV <villalow@kean.edu>
2024-04-03 22:00:53 -0400

    Final for Lab 6

--- PreOrderBST.java
+++ PreOrderBST.java
@@ -1,3 +1,11 @@
+
+/*******************************************************************************
+ * Kean University- Semester (Spring 2024)
+ * Course: CPS*2232*02 - Data Structures
+ * Author(s): Wilbert Villalobos CPS2232*02
+ * Assignment- 
+ *************************************************************************/
+
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.Stack;
@@ -7,17 +15,14 @@ public class PreOrderBST<E extends Comparable<E>> implements Tree<E> {
     protected int size = 0;
     protected java.util.Comparator<E> c;
 
-    /** Create a default BST with a natural order comparator */
     public PreOrderBST() {
-        this.c = (e1, e2) -> ((Comparable<E>)e1).compareTo(e2);
+        this.c = (e1, e2) -> ((Comparable<E>) e1).compareTo(e2);
     }
 
-    /** Create a BST with a specified comparator */
     public PreOrderBST(java.util.Comparator<E> c) {
         this.c = c;
     }
 
-    /** Create a binary tree from an array of objects */
     public PreOrderBST(E[] objects) {
         for (E object : objects) {
             insert(object);
@@ -56,7 +61,7 @@ public class PreOrderBST<E extends Comparable<E>> implements Tree<E> {
                     parent = current;
                     current = current.right;
                 } else {
-                    return false; // Duplicate node not inserted
+                    return false;
                 }
             }
             if (c.compare(e, parent.element) < 0) {
@@ -66,7 +71,7 @@ public class PreOrderBST<E extends Comparable<E>> implements Tree<E> {
             }
         }
         size++;
-        return true; // Element inserted successfully
+        return true;
     }
 
     protected TreeNode<E> createNewNode(E e) {
@@ -79,7 +84,8 @@ public class PreOrderBST<E extends Comparable<E>> implements Tree<E> {
     }
 
     protected void inorder(TreeNode<E> root) {
-        if (root == null) return;
+        if (root == null)
+            return;
         inorder(root.left);
         System.out.print(root.element + " ");
         inorder(root.right);
@@ -91,7 +97,8 @@ public class PreOrderBST<E extends Comparable<E>> implements Tree<E> {
     }
 
     protected void postorder(TreeNode<E> root) {
-        if (root == null) return;
+        if (root == null)
+            return;
         postorder(root.left);
         postorder(root.right);
         System.out.print(root.element + " ");
@@ -103,7 +110,8 @@ public class PreOrderBST<E extends Comparable<E>> implements Tree<E> {
     }
 
     protected void preorder(TreeNode<E> root) {
-        if (root == null) return;
+        if (root == null)
+            return;
         System.out.print(root.element + " ");
         preorder(root.left);
         preorder(root.right);
@@ -127,8 +135,6 @@ public class PreOrderBST<E extends Comparable<E>> implements Tree<E> {
     public TreeNode<E> getRoot() {
         return root;
     }
-
-    // Other methods...
 
     @Override
     public Iterator<E> iterator() {
@@ -167,72 +173,69 @@ public class PreOrderBST<E extends Comparable<E>> implements Tree<E> {
 
     @Override
     public void clear() {
-        root = null; // Remove all elements by setting root to null
-        size = 0;    // Reset size to 0
+        root = null;
+        size = 0;
     }
-    
 
-    @Override /** Delete an element from the binary tree.
-    * Return true if the element is deleted successfully
-    * Return false if the element is not in the tree */
-   public boolean delete(E e) {
-     // Locate the node to be deleted and also locate its parent node
-     TreeNode<E> parent = null;
-     TreeNode<E> current = root;
-     while (current != null) {
-       if (c.compare(e, current.element) < 0) {
-         parent = current;
-         current = current.left;
-       }
-       else if (c.compare(e, current.element) > 0) {
-         parent = current;
-         current = current.right;
-       }
-       else
-         break; // Element is in the tree pointed at by current
-     }
- 
-     if (current == null)
-       return false; // Element is not in the tree
- 
-     // Case 1: current has no left child
-     if (current.left == null) {
-       // Connect the parent with the right child of the current node
-       if (parent == null) {
-         root = current.right;
-       }
-       else {
-         if (c.compare(e, parent.element) < 0)
-           parent.left = current.right;
-         else
-           parent.right = current.right;
-       }
-     }
-     else {
-       // Case 2: The current node has a left child
-       // Locate the rightmost node in the left subtree of
-       // the current node and also its parent
-       TreeNode<E> parentOfRightMost = current;
-       TreeNode<E> rightMost = current.left;
- 
-       while (rightMost.right != null) {
-         parentOfRightMost = rightMost;
-         rightMost = rightMost.right; // Keep going to the right
-       }
- 
-       // Replace the element in current by the element in rightMost
-       current.element = rightMost.element;
- 
-       // Eliminate rightmost node
-       if (parentOfRightMost.right == rightMost)
-         parentOfRightMost.right = rightMost.left;
-       else
-         // Special case: parentOfRightMost == current
-         parentOfRightMost.left = rightMost.left;     
-     }
- 
-     size--; // Reduce the size of the tree
-     return true; // Element deleted successfully
-   }
- 
+    @Override /**
+               * Delete an element from the binary tree.
+               * Return true if the element is deleted successfully
+               * Return false if the element is not in the tree
+               */
+    public boolean delete(E e) {
+        // Locate the node to be deleted and also locate its parent node
+        TreeNode<E> parent = null;
+        TreeNode<E> current = root;
+        while (current != null) {
+            if (c.compare(e, current.element) < 0) {
+                parent = current;
+                current = current.left;
+            } else if (c.compare(e, current.element) > 0) {
+                parent = current;
+                current = current.right;
+            } else
+                break; // Element is in the tree pointed at by current
+        }
+
+        if (current == null)
+            return false; // Element is not in the tree
+
+        // Case 1: current has no left child
+        if (current.left == null) {
+            // Connect the parent with the right child of the current node
+            if (parent == null) {
+                root = current.right;
+            } else {
+                if (c.compare(e, parent.element) < 0)
+                    parent.left = current.right;
+                else
+                    parent.right = current.right;
+            }
+        } else {
+            // Case 2: The current node has a left child
+            // Locate the rightmost node in the left subtree of
+            // the current node and also its parent
+            TreeNode<E> parentOfRightMost = current;
+            TreeNode<E> rightMost = current.left;
+
+            while (rightMost.right != null) {
+                parentOfRightMost = rightMost;
+                rightMost = rightMost.right; // Keep going to the right
+            }
+
+            // Replace the element in current by the element in rightMost
+            current.element = rightMost.element;
+
+            // Eliminate rightmost node
+            if (parentOfRightMost.right == rightMost)
+                parentOfRightMost.right = rightMost.left;
+            else
+                // Special case: parentOfRightMost == current
+                parentOfRightMost.left = rightMost.left;
+        }
+
+        size--; // Reduce the size of the tree
+        return true; // Element deleted successfully
+    }
+
 }
--- TestBSTCollections.java
+++ TestBSTCollections.java
@@ -1,13 +1,26 @@
+
+/*******************************************************************************
+ * Kean University- Semester (Spring 2024)
+ * Course: CPS*2232*02 - Data Structures
+ * Author(s): Wilbert Villalobos CPS2232*02
+ * Assignment- HW 6
+ *************************************************************************/
+
 import java.util.Collections;
 
 public class TestBSTCollections {
   public static void main(String[] args) {
-      BST<Integer> bst = new BST<>();
-      // Populate bst with your Kean ID digits, avoiding duplicates
-      bst.insert(1); bst.insert(2); bst.insert(2); bst.insert(8); bst.insert(0); bst.insert(6); bst.insert(8);
+    BST<Integer> bst = new BST<>();
+    bst.insert(1);
+    bst.insert(2);
+    bst.insert(2);
+    bst.insert(8);
+    bst.insert(0);
+    bst.insert(6);
+    bst.insert(8);
 
-      System.out.println("Value of maximum element in the tree is: " + Collections.max(bst));
-      System.out.println("Value of minimum element in the tree is: " + Collections.min(bst));
-      System.out.println("Frequency of 8 in the tree: " + Collections.frequency(bst, 8));
+    System.out.println("Value of maximum element in the tree is: " + Collections.max(bst));
+    System.out.println("Value of minimum element in the tree is: " + Collections.min(bst));
+    System.out.println("Frequency of 8 in the tree: " + Collections.frequency(bst, 8));
   }
 }
